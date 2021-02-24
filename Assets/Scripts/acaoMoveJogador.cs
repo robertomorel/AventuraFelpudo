a8a904318a9beebb9338c695fa885d1327e429e1
Roberto Morel <roberto.morel@hp.com>
2021-02-23 21:58:36 -0300

    Primeiro commit

---
+++ Assets/Scripts/acaoMoveJogador.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class acaoMoveJogador : MonoBehaviour
+{
+
+    private float velocidade = 1.0f;
+    private float giro = 180.0f;
+    private float gravidade = 3.5f;
+    private float pulo = 6.0f;
+    private CharacterController objetoCharControler;
+    private Vector3 vetorDirecao = new Vector3(0, 0, 0);
+
+    public GameObject jogador;
+    public Animation animacao;
+
+    void Start()
+    {
+        objetoCharControler = GetComponent<CharacterController>();
+        animacao = jogador.GetComponent<Animation>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            velocidade = 2.5f;
+        }
+        else
+        {
+            velocidade = 5;
+        }
+
+        Vector3 forward = Input.GetAxis("Vertical") * transform.TransformDirection(Vector3.forward) * velocidade;
+        transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * giro * Time.deltaTime, 0));
+
+        objetoCharControler.Move(forward * Time.deltaTime);
+        objetoCharControler.SimpleMove(Physics.gravity);
+
+        if (Input.GetButton("Jump"))
+        {
+            if (objetoCharControler.isGrounded == true)
+            {
+                vetorDirecao.y = pulo;
+            }
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            if (objetoCharControler.isGrounded == true)
+            {
+                vetorDirecao.y = pulo;
+                jogador.GetComponent<Animation>().Play("JUMP");
+            }
+        }
+        else
+        {
+            if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+            {
+                if (!animacao.IsPlaying("JUMP"))
+                {
+                    jogador.GetComponent<Animation>().Play("WALK");
+                }
+
+
+
+            }
+            else
+            {
+                if (objetoCharControler.isGrounded == true)
+                {
+                    jogador.GetComponent<Animation>().Play("IDLE");
+                }
+            }
+        }
+
+        vetorDirecao.y -= gravidade * Time.deltaTime;
+        objetoCharControler.Move(vetorDirecao * Time.deltaTime);
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+<img src="https://raw.githubusercontent.com/robertomorel/assets/master/unity.jpeg">
+
+# Adventure Felpudo - Desktop Game
+This is a game project developed using Unity 3D engine and c# as script language. 
+
+Download the last version on Unity Hub [here](https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup.exe).
+
+## About
+This is a 3D free style adventure game, which objective a seek of pieces on a open scenario.
+
+Created to run on a desktop environment
+
+## Features
+- Find pieces 
+- Open scenario
+- 3D objects 
+- Catching points
+
+---
+
+#### Developed By
+
+Roberto Morel - __Passionate by development__
+
+[Let´s talk](https://www.linkedin.com/in/roberto-morel-6b9065193/)
